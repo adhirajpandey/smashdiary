6306101c3668a544ef929f23219e38db28b5e680
adhirajpandey <pandey.adhiraj02@gmail.com>
2026-09-24 19:37:17 +0530

    chore: remove unused modules, components, and page hero styles

--- src/lib/store.ts
+++
@@ -1,8 +0,0 @@
-import { saveMatch } from "@/lib/commands/save-match";
-import { getMatchByIdQuery, listMatchesQuery } from "@/lib/queries/matches";
-import { listPlayersQuery } from "@/lib/queries/players";
-
-export const listPlayers = listPlayersQuery;
-export const listGames = listMatchesQuery;
-export const getGameById = getMatchByIdQuery;
-export const saveGame = saveMatch;
